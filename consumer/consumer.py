0b86c497245642c38db05c9f19be1ee54df01e33
LaisHatsumiSato <56139707+LaisHatsumiSato@users.noreply.github.com>
2026-03-29 23:03:38 +0000

    correção

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -27,7 +27,7 @@ for i in range(10):
         cursor = conn.cursor()
 
         # 🔥 GARANTE SCHEMA
-        cursor.execute("SET search_path TO bronze;")
+        cursor.execute("SET search_path TO staging;")
         conn.commit()
 
         print("✅ Conectado ao PostgreSQL!")
@@ -62,59 +62,17 @@ if consumer is None:
     raise Exception("❌ Não conectou no Kafka")
 
 # -------------------------------
-def processar_dados(dados):
-    try:
-        return {
-            "transacao_id": dados.get("transacao_id"),
-            "cliente_id": int(dados["cliente_id"]) if dados.get("cliente_id") else None,
-            "produto": dados.get("produto"),
-            "categoria": dados.get("categoria"),
-            "valor": float(dados["valor"]) if dados.get("valor") else None,
-            "quantidade": int(dados["quantidade"]) if dados.get("quantidade") else None,
-            "data_transacao": (
-                datetime.fromisoformat(dados["data_transacao"])
-                if dados.get("data_transacao")
-                else None
-            ),
-            "metodo_pagamento": dados.get("metodo_pagamento"),
-        }
-
-    except Exception as e:
-        print("❌ Erro ao processar:", e)
-        return {}
-
-# -------------------------------
 def salvar_lote(lista_dados):
     try:
-        registros = [
-            (
-                d.get("transacao_id"),
-                d.get("cliente_id"),
-                d.get("produto"),
-                d.get("categoria"),
-                d.get("valor"),
-                d.get("quantidade"),
-                d.get("data_transacao"),
-                d.get("metodo_pagamento")
-            )
-            for d in lista_dados
-        ]
+        registros = [(json.dumps(d),) for d in lista_dados]
 
         cursor.executemany("""
-            INSERT INTO bronze.transacoes (
-                transacao_id,
-                cliente_id,
-                produto,
-                categoria,
-                valor,
-                quantidade,
-                data_transacao,
-                metodo_pagamento
-            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO staging.transacoes_raw (payload)
+            VALUES (%s)
         """, registros)
 
         conn.commit()
-        print(f"🚀 Inserido lote com {len(lista_dados)} registros!")
+        print(f"🚀 Inserido lote RAW com {len(lista_dados)} registros!")
 
     except Exception as e:
         conn.rollback()
@@ -132,13 +90,8 @@ for message in consumer:
     dados = message.value
 
     print(f"📥 Recebendo... total buffer: {len(buffer)}")
-
-    dados_processados = processar_dados(dados)
-
-    if not dados_processados:
-        continue
-
-    buffer.append(dados_processados)
+    # agora salva direto o JSON
+    buffer.append(dados)
 
     # 🔥 condição 1: lote cheio
     if len(buffer) >= BATCH_SIZE:
